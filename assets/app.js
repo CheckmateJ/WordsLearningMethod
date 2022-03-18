f81394bc7f6ceeba6e84bd50ae7c6ad5c2e1c9d8
Michal <matej50@gmail.com>
2022-03-18 23:22:40 +0100

    fixed feature repetition

--- assets/app.js
+++ assets/app.js
@@ -13,7 +13,6 @@ window.addEventListener('load', function () {
     }
 })
 
-
 document
     .querySelectorAll('.add_item_link')
     .forEach(btn => {
@@ -41,7 +40,6 @@ document
 
     });
 
-
 const addTagFormDeleteLink = (li) => {
     const removeFormButton = document.createElement('button');
     removeFormButton.className = 'btn btn-danger mt-4';
@@ -55,33 +53,32 @@ const addTagFormDeleteLink = (li) => {
 }
 
 let card = document.querySelector('.course-card');
-let words, keys, courseId, front, previous;
+let words, keys, courseId, front, previous, repetitionId, translationId;
 let i = 1;
 document.querySelectorAll(".add-repetition").forEach(btn => {
     btn.addEventListener('click', function (e) {
-        courseId = btn.dataset.id
-        let repetition, slideId;
-        slideId = btn.dataset.slideid;
-        console.log(btn.dataset.repetition == '0', btn.innerText == "Don't know")
-        if (btn.dataset.repetition == '0' && (btn.innerText == "Don't know" || btn.innerText == "Almost")) {
+        courseId = btn.dataset.courseId
+        let repetition;
+        translationId = btn.previousElementSibling.dataset.translationId
+        if (btn.dataset.repetition === '0' && (btn.innerText === "Don't know" || btn.innerText === "Almost")) {
             repetition = 1;
-        }else if(btn.dataset.repetition == '0'){
+        } else if (btn.dataset.repetition === '0') {
             repetition = 3;
         }
-        console.log(slideId)
-        getWordsFromCourse(btn, courseId, false ,repetition, slideId);
+        getWordsFromCourse(btn, courseId, false, repetition, translationId);
     })
 })
+
 document.querySelectorAll('.course-card').forEach(card => {
     card.addEventListener('click', function () {
-        courseId = card.dataset.id;
+        courseId = card.dataset.courseId;
         let fromCard = true;
         getWordsFromCourse(card, courseId, fromCard);
     })
 })
 
-function getWordsFromCourse(element, id, fromCard , repetition = null, slideId = null) {
-    const data = {'courseId': id, 'repetition': repetition, 'id': slideId};
+function getWordsFromCourse(element, id, fromCard, repetition = null, translationId = null) {
+    const data = {'courseId': id, 'repetition': repetition, 'id': translationId};
     fetch('/course/flashcards', {
         method: 'POST', // or 'PUT'
         headers: {
@@ -93,23 +90,42 @@ function getWordsFromCourse(element, id, fromCard , repetition = null, slideId =
         .then(data => {
             words = [];
             keys = [];
-            words = data;
+            words = data[0];
+            repetitionId = data[1];
             if (!front) {
-                previous = card.innerText;
-                card.innerText = words[previous];
-                front = true;
+                // previous = card.innerText;
+                // card.innerText = words[previous];
+                // card.dataset.translationId =
+                // front = true;
+                // card.dataset.translationId = repetitionId[card.innerText];
             } else {
                 card.innerText = previous;
                 front = false;
             }
             if (!fromCard) {
                 keys = Object.keys(words);
+                console.log(keys)
+                card.dataset.translationId = repetitionId[keys[i]];
                 card.innerText = keys[i];
                 previous = card.innerText
-                i++;
             }
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 }
+
+/*
+ 1. Refactoring
+ 2. Check names
+ 3. Add field in entity => repetitionDate
+ 4. Based on  button repetition add repetition to date
+ 5. When i fetching data from db check fieldRepetitionDate if This field is empty fetch this word
+
+
+    Repetitions:
+    1. Fix the tabs with courses
+    2. For each course add button repetition
+    3. Check in corntroller if repetitionDate in that course  equals = today if yes count this word and add to new array ?
+
+ */
